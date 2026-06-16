82a152217aa2116949458479e2d91670408e9ebf
catherine4331 <catherine4331@gmail.com>
2026-06-16 18:31:54 +0100

    Pre process input text

--- src/tokenize.py
+++ src/tokenize.py
@@ -1,4 +1,5 @@
 import urllib.request
+import re
 
 url = ("https://raw.githubusercontent.com/rasbt/"
 "LLMs-from-scratch/main/ch02/01_main-chapter-code/"
@@ -8,5 +9,8 @@ urllib.request.urlretrieve(url, file_path)
 
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
-print("Total number of character:", len(raw_text))
-print(raw_text[:99])
+
+preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+preprocessed = [item.strip() for item in preprocessed if item.strip()]
+print(len(preprocessed))
+print(preprocessed[:30])
