a5a0b7fc93f0805656d04e663a4081e3792ee80f
catherine4331 <catherine4331@gmail.com>
2026-06-16 18:44:11 +0100

    Add a tokenizer class to encode and decode text

--- src/tokenize.py
+++ src/tokenize.py
@@ -1,6 +1,25 @@
 import urllib.request
 import re
 
+class SimpleTokenizerV1:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i:s for s, i in vocab.items()}
+
+    def encode(self, text):
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+        preprocessed = [
+            item.strip() for item in preprocessed if item.strip()
+        ]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        return text
+
 url = ("https://raw.githubusercontent.com/rasbt/"
 "LLMs-from-scratch/main/ch02/01_main-chapter-code/"
 "the-verdict.txt")
@@ -10,7 +29,17 @@ urllib.request.urlretrieve(url, file_path)
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
 
+
 preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
 preprocessed = [item.strip() for item in preprocessed if item.strip()]
-print(len(preprocessed))
-print(preprocessed[:30])
+
+# Create vocabulary
+all_words = sorted(set(preprocessed))
+vocab = {token:integer for integer, token in enumerate(all_words)}
+
+tokenizer = SimpleTokenizerV1(vocab)
+text = """"It's the last he painted, you know,"
+Mrs. Gisburn said with pardonable pride."""
+ids = tokenizer.encode(text)
+print(ids)
+print(tokenizer.decode(ids))
