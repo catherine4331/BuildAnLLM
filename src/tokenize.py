b15c4ed5a439813661d715ddd3ccdec7f58f6ebe
catherine4331 <catherine4331@gmail.com>
2026-06-16 18:49:01 +0100

    Handle unknown tokens and file separators

--- src/tokenize.py
+++ src/tokenize.py
@@ -11,13 +11,15 @@ class SimpleTokenizerV1:
         preprocessed = [
             item.strip() for item in preprocessed if item.strip()
         ]
+        preprocessed = [item if item in self.str_to_int
+                        else "<|unk|>" for item in preprocessed]
         ids = [self.str_to_int[s] for s in preprocessed]
         return ids
 
     def decode(self, ids):
         text = " ".join([self.int_to_str[i] for i in ids])
 
-        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        text = re.sub(r'\s+([,.:;?!"()\'])', r'\1', text)
         return text
 
 url = ("https://raw.githubusercontent.com/rasbt/"
@@ -34,12 +36,12 @@ preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
 preprocessed = [item.strip() for item in preprocessed if item.strip()]
 
 # Create vocabulary
-all_words = sorted(set(preprocessed))
-vocab = {token:integer for integer, token in enumerate(all_words)}
+all_tokens = sorted(list(set(preprocessed)))
+all_tokens.extend(["<|endoftext|>", "<|unk|>"])
+vocab = {token:integer for integer,token in enumerate(all_tokens)}
 
 tokenizer = SimpleTokenizerV1(vocab)
-text = """"It's the last he painted, you know,"
-Mrs. Gisburn said with pardonable pride."""
-ids = tokenizer.encode(text)
-print(ids)
-print(tokenizer.decode(ids))
+text1 = "Hello, do you like tea?"
+text2 = "In the sunlit terraces of the palace."
+text = " <|endoftext|> ".join((text1, text2))
+print(tokenizer.decode(tokenizer.encode(text)))
