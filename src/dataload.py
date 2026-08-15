97562785de3b3c75c0dd9b79077c829a8a414e4d
catherine4331 <catherine4331@gmail.com>
2026-08-15 19:30:39 +0100

    Add some embeddings

--- src/dataload.py
+++ src/dataload.py
@@ -38,9 +38,13 @@ def create_dataloader_v1(txt, batch_size=4, max_length=256,
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
 
-dataloader = create_dataloader_v1(raw_text, batch_size=1, max_length=8, stride=2, shuffle=False)
+vocab_size = 50257
+output_dim = 256
+token_embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+
+max_length = 4
+dataloader = create_dataloader_v1(raw_text, batch_size=8, max_length=max_length, stride=max_length, shuffle=False)
 data_iter = iter(dataloader)
-first_batch = next(data_iter)
-print(first_batch)
-second_batch = next(data_iter)
-print(second_batch)
+inputs, targets = next(data_iter)
+print("Token IDs:\n", inputs)
+print("\nInputs shape:\n", inputs.shape)
