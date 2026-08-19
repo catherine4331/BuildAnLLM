f857a14a6378fa9f47fc4aeaf2d0d3db388c0317
catherine4331 <catherine4331@gmail.com>
2026-08-19 21:11:07 +0100

    Weighted attention

---
+++ src/weighted_attention.py
@@ -0,0 +1,34 @@
+import torch
+
+inputs = torch.tensor(
+[[0.43, 0.15, 0.89], # Your (x^1)
+[0.55, 0.87, 0.66], # journey (x^2)
+[0.57, 0.85, 0.64], # starts (x^3)
+[0.22, 0.58, 0.33], # with (x^4)
+[0.77, 0.25, 0.10], # one (x^5)
+[0.05, 0.80, 0.55]] # step (x^6)
+)
+
+x_2 = inputs[1]
+d_in = inputs.shape[1]
+d_out = 2
+
+# Initialise our weight matrices
+torch.manual_seed(123)
+W_query = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+W_key   = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+W_value = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+
+query_2 = x_2 @ W_query
+key_2 = x_2 @ W_key
+value_2 = x_2 @ W_value
+
+# We need all the key and value vectors to calculate the context vector for x^2
+keys = inputs @ W_key
+values = inputs @ W_value
+
+# The attention score for each token is the dot product of the query vector with the key vector
+# Note these are the transformed input vectors that have gone through out weight matrices
+keys_2 = keys[1]
+attn_score_22 = query_2.dot(keys_2)
+print(attn_score_22)
