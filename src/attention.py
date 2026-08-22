8c1fb18e46dffd421b5446ce367b295a42c55ffb
catherine4331 <catherine4331@gmail.com>
2026-08-22 10:35:52 +0100

    A better weighted attention class using linear

--- src/attention.py
+++ src/attention.py
@@ -18,3 +18,42 @@ class SelfAttention_v1(nn.Module):
         )
         context_vec = attn_weights @ values
         return context_vec
+
+class SelfAttention_v2(nn.Module):
+    def __init__(self, d_in, d_out, qkv_bias=False):
+        super().__init__()
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+
+    def forward(self, x):
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape[-1]**0.5, dim=-1
+        )
+        context_vec = attn_weights @ values
+        return context_vec
+
+inputs = torch.tensor(
+[[0.43, 0.15, 0.89], # Your (x^1)
+[0.55, 0.87, 0.66], # journey (x^2)
+[0.57, 0.85, 0.64], # starts (x^3)
+[0.22, 0.58, 0.33], # with (x^4)
+[0.77, 0.25, 0.10], # one (x^5)
+[0.05, 0.80, 0.55]] # step (x^6)
+)
+d_in = inputs.shape[1]
+d_out = 2
+
+sa_1 = SelfAttention_v1(d_in, d_out)
+sa_2 = SelfAttention_v2(d_in, d_out)
+
+sa_1.W_key = nn.Parameter(sa_2.W_key.weight.T)
+sa_1.W_query = nn.Parameter(sa_2.W_query.weight.T)
+sa_1.W_value = nn.Parameter(sa_2.W_value.weight.T)
+
+print(sa_1(inputs))
+print(sa_2(inputs))
