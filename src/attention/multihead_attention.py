64d7fa7795f8f21bbff858373d14c189ede2cc4b
catherine4331 <catherine4331@gmail.com>
2026-09-06 19:33:08 +0100

    Move files

--- src/attention/multihead_attention.py
+++ src/attention/multihead_attention.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from causal_attention import CausalAttention
+from attention.causal_attention import CausalAttention
 
 class MultiHeadAttentionWrapper(nn.Module):
     def __init__(self, d_in, d_out, context_length,
