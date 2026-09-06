8c96a24ba6194c4a5e409c5065ed2abc341082dd
catherine4331 <catherine4331@gmail.com>
2026-09-06 20:46:15 +0100

    Add layer normalisation

--- src/transformer/gpt.py
+++ src/transformer/gpt.py
@@ -6,8 +6,8 @@ GPT_CONFIG_124M = {
     "vocab_size": 50257,
     "context_length": 1024,
     "emb_dim": 768,
-    "n_head": 12,
-    "n_layers": 12,
+    "num_head": 12,
+    "num_layers": 12,
     "drop_rate": 0.1,
     "qkv_bias": False
 }
@@ -54,6 +54,19 @@ class DummyLayerNorm(nn.Module):
     def forward(self, x):
         return x
 
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
+
 tokenizer = tiktoken.get_encoding("gpt2")
 batch = []
 txt1 = "Every effort moves you"
@@ -62,4 +75,9 @@ txt2 = "Every day holds a"
 batch.append(torch.tensor(tokenizer.encode(txt1)))
 batch.append(torch.tensor(tokenizer.encode(txt2)))
 batch = torch.stack(batch, dim=0)
-print(batch)
+
+torch.manual_seed(123)
+model = DummyGPTModel(GPT_CONFIG_124M)
+logits = model(batch)
+print("Output shape:", logits.shape)
+print(logits)
